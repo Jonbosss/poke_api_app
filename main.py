1701f2b67c62c1e76497e3ac5f06f5b3ad0ec3d5
Jonbosss <jonathanfarhat@gmail.com>
2025-10-31 13:32:58 -0700

    implemented feature to open an image of flareon. Working on fix to make it display any pokemon entered.

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import requests
+from PIL import Image
+from io import BytesIO
 
 base_url = "https://pokeapi.co/api/v2/"
+image_url = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/136.png"
 
 
 def get_pokemon_info(name):
@@ -14,17 +17,33 @@ def get_pokemon_info(name):
         print(f"Failed to retrieve data {response.status_code}")
 
 
+# def display_pokemon_image(id):
+image_response = requests.get(image_url)
+
+# if response.status_code == 200:
+#     pokemon_data = response.json()
+#     return pokemon_data
+# else:
+#     print(f"Failed to retrieve data {response.status_code}")
+#
+#
 pokemon_name = "flareon"
 pokemon_info = get_pokemon_info(pokemon_name)
+img = Image.open(BytesIO(image_response.content))
+
+# f"{pokemon_info["sprites"]["front_default"]}"
+# pokemon_image = get_pokemon_image("id")
 
 if pokemon_info:
     print(f"Name: {pokemon_info["name"].capitalize()}")
-    print(f"Id: {pokemon_info["id"]}")
-    print(f"Height: {pokemon_info["height"]}")
-    print(f"Weight: {pokemon_info["weight"]}")
+    print(f"# {pokemon_info["id"]}")
     if (len(pokemon_info["types"])) > 1:
         print(
-        f"Types: {pokemon_info["types"][0]["type"]["name"].capitalize()}" f" and {pokemon_info["types"][1]["type"]["name"].capitalize()}")
+            f"Types: {pokemon_info["types"][0]["type"]["name"].capitalize()}" f" and {pokemon_info["types"][1]["type"]["name"].capitalize()}")
     else:
         print(
-        f"Type: {pokemon_info["types"][0]["type"]["name"].capitalize()}")
+            f"Type: {pokemon_info["types"][0]["type"]["name"].capitalize()}")
+    print(f"Height: {pokemon_info["height"]}")
+    print(f"Weight: {pokemon_info["weight"]}")
+    img.show(
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/136.png")
